54ac0d8ddf7e43fd42837981546ae071d5890792
stxh007@gmail.com <stxh007@gmail.com@abb01850-dd6e-11de-aa1f-bbd297b2055c>
2009-12-14 08:23:58 +0000

    多语言支持,用户显示,sgf显示,最后一子显示

--- trunk/lang.js
+++ trunk/lang.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 // lang.js
 langOption = {
-	en : {value: "en"; text: "English";};
-	zh : {value: "zh"; text: "Chinese (Simplified)";};
+	en : {value: "en", text: "English"},
+	zh : {value: "zh", text: "Chinese (Simplified)"}
 }
